c682a4ae8307f9f8ffa69522fe1e181db9057682
ralppppy <ralpyosores@gmail.com>
2020-08-25 15:48:16 +0800

    Updates

---
+++ precache-manifest.f47cb039e79ab24f08f7731f2211b385.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef72c7d2cf0b3117e741b270a8701fa6",
+    "url": "/template-maker/index.html"
+  },
+  {
+    "revision": "6a74209a3bc2c8a9a814",
+    "url": "/template-maker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8d4c79baaf7660d8d6b3",
+    "url": "/template-maker/static/css/main.8edcbbc6.chunk.css"
+  },
+  {
+    "revision": "6a74209a3bc2c8a9a814",
+    "url": "/template-maker/static/js/2.cb9bd8f7.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/template-maker/static/js/2.cb9bd8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d4c79baaf7660d8d6b3",
+    "url": "/template-maker/static/js/main.0c6f46f8.chunk.js"
+  },
+  {
+    "revision": "74c94889691b602404b5",
+    "url": "/template-maker/static/js/runtime-main.2d6f8215.js"
+  },
+  {
+    "revision": "be90085e39a105969881550125b2c034",
+    "url": "/template-maker/static/media/background.be90085e.png"
+  },
+  {
+    "revision": "9b40f293a2c07fadc60d0b4d6709abf6",
+    "url": "/template-maker/static/media/dress.9b40f293.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/template-maker/static/media/logo.5d5d9eef.svg"
+  }
+]);
